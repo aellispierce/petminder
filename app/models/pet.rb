a2a2eb3fc83241680b597108f11bb78e484e1101
Ashley Ellis <anellis12@gmail.com>
2015-02-20 00:33:43 -0500

    Add user validation to pet to get tests to pass

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,5 +2,6 @@ class Pet < ActiveRecord::Base
   validates :name, presence: true
   validates :breed, presence: true
   belongs_to :user
+  validates :user, presence: true
   has_many :tasks
 end
--- spec/factories/pet.rb
+++ spec/factories/pet.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
   factory :pet do
     name { Faker::Name.name }
     breed { Faker::Name.name }
-    association :user_id, factory: :user
+    association :user, factory: :user
   end
 end
